05157a24b803f485435eda8e1ce51583a769fddd
ByMalm <Hisoka88Kproton.me>
2026-09-09 21:05:03 -0600

    feat: update 1.2

--- src/config.ts
+++ src/config.ts
@@ -41,7 +41,7 @@ const WEBSITE_SORTS: WebsiteSort[] = [
         icon: "https://cdn.simpleicons.org/googlegemini/8E75B2",
       },
       {
-        title: "GROQ",
+        title: "GROK",
         description: "GROK",
         url: "	https://grok.com",
         icon: "https://cdn.simpleicons.org/x",
@@ -52,7 +52,7 @@ const WEBSITE_SORTS: WebsiteSort[] = [
     title: "SITE WEBS",
     sites: [
       {
-        title: "nose",
+        title: "GROQ",
         description: "GROQ",
         url: "	https://groq.com",
         icon: "",
@@ -62,39 +62,19 @@ const WEBSITE_SORTS: WebsiteSort[] = [
   },
 ];
 
-/** Website 配置（2023.3.29 已废弃） */
 const WEBSITE_ITEMS: WebsiteItem[] = [];
 
 const GLOBAL_CONFIG = {
-  /**
-   * 博客名称
-   */
-  BLOG_NAME: "EsunR-Blog",
-  /**
-   * 个人博客链接
-   */
-  BLOG_URL: "https://blog.esunr.xyz",
-  /**
-   * 指定中心 LOGO 图片地址
-   */
+  BLOG_NAME: "MALM",
+
+  BLOG_URL: "https://bymalm.com/",
+
   LOGO_URL: null,
-  /**
-   * 个人 Github 链接
-   */
-  GITHUB_URL: "https://github.com/EsunR",
-  /**
-   * 背景图片地址
-   */
+
+  GITHUB_URL: "https://github.com/ByMalm",
+
   BACKGROUND_IMG_URL: "https://api.dujin.org/bing/1920.php",
-  /**
-   * ICP 备案号，留空不显示
-   */
-  ICP: "京ICP证000001号",
-  ICP_URL: "https://beian.miit.gov.cn/",
-  FOOTER_INFO: true,
-  /**
-   * 网站欢迎标语
-   */
+
   SLOGANS: [
     "Bienvenido, mi amigo!",
     "欢迎拜访",
@@ -107,21 +87,11 @@ const GLOBAL_CONFIG = {
     "Benvenuto, caro amico!",
     "Willkommen in unserer Welt!",
   ],
-  /**
-   * Website 分类配置
-   */
-  WEBSITE_SORTS,
-  /**
-   * Website 配置（2023.3.29 已废弃）
-   */
-  WEBSITE_ITEMS,
-  /**
-   * 网站 Title Keywords Description 的配置，用于 SEO
-   */
+
   TKD: {
-    title: "My Blog Index",
-    keywords: "Blog, Index, Index Page",
-    description: "This is my personal blog index page.",
+    title: "MALM",
+    keywords: "",
+    description: "",
   },
 };
 
